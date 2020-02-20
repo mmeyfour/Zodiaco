2f77dbb0cb2ee8cf80f8092f42fceda5e1ed04ad
mersad meyfour asadi <mmeyfouricloud.com>
2020-02-20 22:52:51 +0100

    constrains almost functional

--- Zodiaco/ViewController.swift
+++ Zodiaco/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let prueba = 0
+    let nuevaPrueba = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
