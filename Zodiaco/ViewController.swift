ce408d769905f31b330874259a28cbf6286fcb41
mersad meyfour asadi <mmeyfouricloud.com>
2020-02-21 14:23:46 +0100

    date recognising

--- Zodiaco/ViewController.swift
+++ Zodiaco/ViewController.swift
@@ -13,29 +13,43 @@ class ViewController: UIViewController {
     var userAge = ""
     var userMonth = ""
     var userDay = ""
+    var userAgeConverted : Double?
+    var userMonthConverted : Double?
+    var userDayConverted : Double?
     var dateConverted = ""
     
+    
+    
+    
     // MARK: - IBOutlets
-    @IBOutlet weak var firestQuestion: UILabel!
+    @IBOutlet weak var dateQuestion: UILabel!
     @IBOutlet weak var dayLabel: UITextField!
     @IBOutlet weak var monthLabel: UITextField!
     @IBOutlet weak var ageLabel: UITextField!
+    @IBOutlet weak var goButton: UIButton!
     
     
     // MARK: - IBAction
     
     @IBAction func didTapButton(_ sender: Any) {
-        userAge = dayLabel.text!
+        userAge = ageLabel.text!
         userMonth = monthLabel.text!
-        userDay = ageLabel.text!
-        dateConverted = userAge + userMonth + userDay
+        userDay = dayLabel.text!
+        dateConverted = "\(userAge)" + "\(userMonth)" + "\(userDay)"
         print("su fecha de cumpleaños es \(dateConverted)")
+        
+        guard let dateInDouble = Double(dateConverted) else {return}
+        if dateInDouble < 19900101 || dateInDouble > 20001231 {
+            goButton.isEnabled = false
+        }
+        
     }
     
     
     // MARK: - Function
     override func viewDidLoad() {
         super.viewDidLoad()
+               goButton.isEnabled = false
         // Do any additional setup after loading the view.
     }
 
