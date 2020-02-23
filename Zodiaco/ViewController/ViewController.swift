7028521585d2578afcf2aca5cb87592529be291e
Mersad Meyfour asadi <Mmeyfour@iCloud.com>
2020-02-23 20:59:55 +0100

    date Converter fixed !!!

--- Zodiaco/ViewController/ViewController.swift
+++ Zodiaco/ViewController/ViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var userAge = ""
-    var userMonth = 0
-    var userDay = 0
-    
-    
     
     // MARK: - IBOutlets
     @IBOutlet weak var dateQuestion: UILabel!
@@ -30,7 +25,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didTapGoButton(_ sender: UIButton) {
-    
+        dateConvert(datePicker.date)
     }
     
     // MARK: - Function
@@ -39,7 +34,18 @@ class ViewController: UIViewController {
         goButton.isEnabled = false
         // Do any additional setup after loading the view.
     }
-
+    
+    func dateConvert(_ date: Date){
+        let dateInString = date.description
+        let dateSpliteinBlank = dateInString.split(separator: " ")
+        let dateSplited = dateSpliteinBlank[0].split(separator: "-")
+        let year = Int(dateSplited[0])
+        let month = Int(dateSplited[1])
+        let day = Int(dateSplited[2])
+        print(year ?? 0)
+        print(month ?? 0)
+        print(day ?? 0)
+    }
     
 }
 
