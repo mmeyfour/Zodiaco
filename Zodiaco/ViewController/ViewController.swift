fc1f1f9f79d3c37bad74072de7d203b5462011c6
Mersad Meyfour asadi <Mmeyfour@iCloud.com>
2020-02-23 18:39:46 +0100

    nueva rama con date picker

--- Zodiaco/ViewController/ViewController.swift
+++ Zodiaco/ViewController/ViewController.swift
@@ -9,50 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var userAge = ""
-    var userMonth = ""
-    var userDay = ""
-    var userAgeConverted : Double?
-    var userMonthConverted : Double?
-    var userDayConverted : Double?
-    var dateConverted = ""
     
+    var userAge = ""
+    var userMonth = 0
+    var userDay = 0
     
     
     
     // MARK: - IBOutlets
     @IBOutlet weak var dateQuestion: UILabel!
-    @IBOutlet weak var dayLabel: UITextField!
-    @IBOutlet weak var monthLabel: UITextField!
-    @IBOutlet weak var ageLabel: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var goButton: UIButton!
     
     
     // MARK: - IBAction
     
-    @IBAction func didTapButton(_ sender: Any) {
-        userAge = ageLabel.text!
-        userMonth = monthLabel.text!
-        userDay = dayLabel.text!
-        dateConverted = "\(userAge)" + "\(userMonth)" + "\(userDay)"
-        print("su fecha de cumpleaños es \(dateConverted)")
-        
-        guard let dateInDouble = Double(dateConverted) else {return}
-        if dateInDouble < 19900101 || dateInDouble > 20001231 {
-            goButton.isEnabled = false
-        }
+    @IBAction func userDidPickDate(_ sender: UIDatePicker) {
+        goButton.isEnabled = true
         
     }
     
+    @IBAction func didTapGoButton(_ sender: UIButton) {
+    
+    }
     
     // MARK: - Function
     override func viewDidLoad() {
         super.viewDidLoad()
-               goButton.isEnabled = false
+        goButton.isEnabled = false
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
