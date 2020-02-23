c0eb8ef6738b7eec5047d44678b04516be2b2c90
Mersad Meyfour asadi <Mmeyfour@iCloud.com>
2020-02-23 22:01:11 +0100

    user page created

---
+++ Zodiaco/ViewController/UserSignViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserSignViewController.swift
+//  Zodiaco
+//
+//  Created by Mersad Meyfour Asadi on 23/02/2020.
+//  Copyright © 2020 Mersad Meyfour Asadi. All rights reserved.
+//
+
+import UIKit
+
+class UserSignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
