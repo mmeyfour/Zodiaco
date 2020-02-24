c162bd6c8f999a0fbc9b7123e52341ee82441fd0
Mersad Meyfour asadi <Mmeyfour@iCloud.com>
2020-02-24 05:16:56 +0100

    info viewcontroller created

---
+++ Zodiaco/ViewController/infoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  infoViewController.swift
+//  Zodiaco
+//
+//  Created by Mersad Meyfour Asadi on 24/02/2020.
+//  Copyright © 2020 Mersad Meyfour Asadi. All rights reserved.
+//
+
+import UIKit
+
+class infoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
